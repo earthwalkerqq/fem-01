a88560bfa5dd0d3c02cdd30b6d7426b379204dcf
earthwalkerqq <ilya.assanov.03@mail.ru>
2025-06-01 19:41:24 +0300

    add animation

--- src/main.c
+++ src/main.c
@@ -10,25 +10,31 @@
 #include "fem.h"
 #include "formation_mtrx.h"
 
-#define LOAD 25000.
+#define LOAD 25000.  // Приложенная нагрузка
 #define FLT_MAX 100.
 #define KOEF_X 25.
 #define KOEF_Y 14.
-#define WINDTH_WINDOW 1200
-#define HEIGHT_WINDOW 800
-#define STRESS_SCALE 1000.0     // Масштаб для визуализации напряжений
-#define DEFORMATION_SCALE 10.0  // Масштаб для визуализации деформаций
+#define WINDTH_WINDOW 1200          // Высота окна
+#define HEIGHT_WINDOW 800           // Ширина окна
+#define STRESS_SCALE 1000.0         // Масштаб для визуализации напряжений
+#define DEFORMATION_SCALE 10.0      // Масштаб для визуализации деформаций
+#define GLUT_WINDOW_POSITION_X 100  // Позиция окна (по оси x)
+#define GLUT_WINDOW_POSITION_Y 100  // Позиция окна (по оси y)
 
 // Глобальные переменные для управления визуализацией
-static int showDeformed = 1;  // Показывать деформированную модель
-static int showStress = 1;    // Показывать напряжения
-static int showValues = 0;    // Показывать числовые значения
-static double zoom = 1.0;     // Масштаб отображения
+static int showDeformed = 1;           // Показывать деформированную модель
+static int showStress = 1;             // Показывать напряжения
+static int showValues = 0;             // Показывать числовые значения
+static double zoom = 1.0;              // Масштаб отображения
+static float animationProgress = 0.f;  // Шаг анимации
+static int isAnimating = 0;            // Флаг анимации
+static int animationDirection = 1;     // Направление анимации
 
 void drawMashForSolve(int argc, char **argv);
 void drawModel(void);
 void display(void);
 void init(void);
+void updateAnimation(int __attribute__((unused)) value);
 void keyboard(unsigned char key, int __attribute__((unused)) x,
               int __attribute__((unused)) y);
 void renderText(float x, float y, const char *text);
@@ -131,12 +137,10 @@ void drawMashForSolve(int argc, char **argv) {
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
   glutInitWindowSize(WINDTH_WINDOW, HEIGHT_WINDOW);
-  glutInitWindowPosition(100, 100);
+  glutInitWindowPosition(GLUT_WINDOW_POSITION_X, GLUT_WINDOW_POSITION_Y);
   glutCreateWindow("FEM Visualization");
-  // Регистрация callback-функций
   glutDisplayFunc(display);
   glutKeyboardFunc(keyboard);
-  // Отрисовка модели
   init();
   glutMainLoop();
 }
@@ -167,8 +171,8 @@ void drawModel(void) {
       double y = car[1][jt03[j][i] - 1];
       if (showDeformed) {
         if (x != 0. && y != 0) {  // опираясь на схему нагружения образца
-          x += u[jt03[j][i] * 2 - 2] * DEFORMATION_SCALE;
-          y += u[jt03[j][i] * 2 - 1] * DEFORMATION_SCALE;
+          x += u[jt03[j][i] * 2 - 2] * DEFORMATION_SCALE * animationProgress;
+          y += u[jt03[j][i] * 2 - 1] * DEFORMATION_SCALE * animationProgress;
         }
       }
       glVertex2f(KOEF_X + x * zoom / 2., KOEF_Y + y * zoom / 2.);
@@ -185,12 +189,16 @@ void drawModel(void) {
       double y2 = car[1][jt03[(index + 1) % 3][i] - 1];
       if (showDeformed) {
         if (x1 != 0. && y1 != 0) {
-          x1 += u[jt03[index][i] * 2 - 2] * DEFORMATION_SCALE;
-          y1 += u[jt03[index][i] * 2 - 1] * DEFORMATION_SCALE;
+          x1 +=
+              u[jt03[index][i] * 2 - 2] * DEFORMATION_SCALE * animationProgress;
+          y1 +=
+              u[jt03[index][i] * 2 - 1] * DEFORMATION_SCALE * animationProgress;
         }
         if (x2 != 0. && y2 != 0) {
-          x2 += u[jt03[(index + 1) % 3][i] * 2 - 2] * DEFORMATION_SCALE;
-          y2 += u[jt03[(index + 1) % 3][i] * 2 - 1] * DEFORMATION_SCALE;
+          x2 += u[jt03[(index + 1) % 3][i] * 2 - 2] * DEFORMATION_SCALE *
+                animationProgress;
+          y2 += u[jt03[(index + 1) % 3][i] * 2 - 1] * DEFORMATION_SCALE *
+                animationProgress;
         }
       }
       glVertex2f(KOEF_X + x1 * zoom / 2., KOEF_Y + y1 * zoom / 2.);
@@ -235,6 +243,20 @@ void init(void) {
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
 }
+void updateAnimation(int __attribute__((unused)) value) {
+  if (isAnimating) {
+    animationProgress += 0.01f * animationDirection;
+    if (animationProgress >= 1.0f) {
+      animationProgress = 1.0f;
+      animationDirection = -1;
+    } else if (animationProgress <= 0.0f) {
+      animationProgress = 0.0f;
+      animationDirection = 1;
+    }
+    glutPostRedisplay();
+    glutTimerFunc(16, updateAnimation, 0);
+  }
+}
 
 void keyboard(unsigned char key, int __attribute__((unused)) x,
               int __attribute__((unused)) y) {
@@ -253,6 +275,14 @@ void keyboard(unsigned char key, int __attribute__((unused)) x,
       break;
     case '-':  // Уменьшение масштаба
       zoom /= 1.1;
+      break;
+    case 'a':  // Включение/выключение анимации
+      isAnimating = !isAnimating;
+      if (isAnimating) {
+        animationProgress = 0.0f;
+        animationDirection = 1;
+        glutTimerFunc(16, updateAnimation, 0);
+      }
       break;
   }
   glutPostRedisplay();
